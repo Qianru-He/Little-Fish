01af83892b4f42434c347253c55858983de1d22b
Qianru-He <47182126+Qianru-He@users.noreply.github.com>
2019-02-21 22:49:22 +0800

    Add files via upload

---
+++ taxi-fee/spec/main-spec.js
@@ -0,0 +1,8 @@
+const main = require('../main/main');
+
+describe('taxi fee',function(){console.log(Math.trunc(fun1.billing(1,10)+0.5));
+console.log(Math.trunc(fun1.billing(2,3)+0.5));
+console.log(Math.trunc(fun1.billing(5,3)+0.5));
+console.log(Math.trunc(fun1.billing(8,3)+0.5));
+console.log(Math.trunc(fun1.billing(10,3)+0.5));
+console.log(Math.trunc(fun1.billing(-1,10)+0.5));});
