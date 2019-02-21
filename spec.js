63bf3eaa8df248959459c51c9e46b21c01569340
Qianru-He <47182126+Qianru-He@users.noreply.github.com>
2019-02-21 22:51:17 +0800

    Delete spec.js

--- spec.js
+++
@@ -1,6 +0,0 @@
-var fun1 = require('./main');
-console.log(Math.trunc(fun1.billing(1,10)+0.5));
-console.log(Math.trunc(fun1.billing(2,3)+0.5));
-console.log(Math.trunc(fun1.billing(5,3)+0.5));
-console.log(Math.trunc(fun1.billing(8,3)+0.5));
-console.log(Math.trunc(fun1.billing(10,3)+0.5));
