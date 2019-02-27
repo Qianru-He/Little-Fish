f1350329ebb6de7d7e2a91b74248fc5cc2a424fe
Qianru-He <47182126+Qianru-He@users.noreply.github.com>
2019-02-27 19:09:16 +0800

    Update main-spec.js

--- taxi-fee/spec/main-spec.js
+++ taxi-fee/spec/main-spec.js
@@ -1,14 +1,14 @@
 var billing = require('../main/main.js');
 
 describe('taxi fee',function(){
-    it("should generate taxi fee when kilo<2",function () {
+    it("should generate taxi fee when kilo<=2",function () {
         let result=billing(1,10);
         let expected=9;
         expect(result).toEqual(expected);
     })
-    it("should generate taxi fee when kilo=2&&<8",function () {
-        let result=billing(2,2);
-        let expected=7;
+    it("should generate taxi fee when kilo>2&&<8",function () {
+        let result=billing(3,2);
+        let expected=9;
         expect(result).toEqual(expected);
     })
     it("should generate taxi fee when kilo=8",function () {
